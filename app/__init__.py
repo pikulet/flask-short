baffdca1e1a484ea99ab8e0a34c292728e6e17da
joyce <jycyeo@yahoo.com.sg>
2020-10-03 22:42:51 +0800

    change to deploy config

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import Config
+from config import DevelopmentConfig, DeployConfig
 from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
      app = Flask(__name__)
-     app.config.from_object(Config)
+     app.config.from_object(DeployConfig)
      app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
      Bootstrap(app)
      return app
